7e82e357ce9d0f94bd1136b361cc00e71de0bfc5
Radin <radin44@protonmail.com>
2025-11-07 15:35:19 -0800

    Added user inputs

--- bode_plot.py
+++ bode_plot.py
@@ -3,8 +3,22 @@ import numpy as np                # Import numerical library for arrays and math
 from scipy import signal          # From the scipy library, just bring in the signal module
 
 # This is the complete Transfer function H(s) = 0.2*(s + 10)/(s*(s + 2))
-numerator = [0.2, 2]       # This is essentially the distributed form of the numerator: 0.2*s + 2
-denominator = [1, 2, 0]    # Similar to above but for 2nd order polynomial of denominator: s^2 + 2*s + 0
+
+# Accept denominator input in a distribtuted Ax^2 + Bx + C format, taking the coefficients as input
+numerator_str = input("Numerator: ")
+numerator_lst = numerator_str.split(",")
+numerator_float = [float(num) for num in numerator_lst]
+print(numerator_float)
+
+# Accept denominator input in a distribtuted Ax^2 + Bx + C format, taking the coefficients as input
+denominator_str = input("Denominator: ")
+denominator_lst = denominator_str.split(",")
+denominator_float = [float(num) for num in denominator_lst]
+print(denominator_float)
+
+
+numerator = numerator_float.copy()      # This is essentially the distributed form of the numerator: 0.2*s + 2
+denominator = denominator_float.copy()    # Similar to above but for 2nd order polynomial of denominator: s^2 + 2*s + 0
 
 # Create transfer function object
 system = signal.TransferFunction(numerator, denominator)    
